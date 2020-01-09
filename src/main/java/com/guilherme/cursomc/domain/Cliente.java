ebf3cd2e4347d6f2ee51d3bf94846251ec095e33
Guilherme Rocha <guilhermerochab97@gmail.com>
2020-01-09 12:09:18 -0300

    Implementação Pedido, Estado e Pagamento

--- src/main/java/com/guilherme/cursomc/domain/Cliente.java
+++ src/main/java/com/guilherme/cursomc/domain/Cliente.java
@@ -33,6 +33,9 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
@@ -103,6 +106,13 @@ public class Cliente implements Serializable {
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	@Override
